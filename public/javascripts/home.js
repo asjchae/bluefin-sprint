d6c3febb9060522ad7ff3e01f8893c6e05eea7a0
Rachel Mathew <rachelmathew999@gmail.com>
2013-03-28 20:45:03 -0400

    Working hide tweets

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,12 +1,32 @@
-//console.log("hello!@");
 $(function(){
 	$(document).ready(function(){
 		$("div").hide();
 	})
+/*	$("#showtweets").click(function(){
+		$("#tweets").show();
+	});
+	$("#hidetweets").click(function(){
+		$("#tweets").hide();
+	});*/
 	$("#article").click(function(){
-		$("div").show();
+		$("#articletext").show();
 	});
 	$("#hide").click(function(){
-		$("div").hide();
+		$("#articletext").hide();
 	});
+	/*$function(d,s,id){
+		var js,fjs=d.getElementsByTagName(s)[0];
+		if(!d.getElementById(id)){
+			js=d.createElement(s);js.id=id;
+			js.src="//platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js,fjs);
+		}*/
+	//}//(document,"script","twitter-wjs");
+	$(function(d,s,id){
+      	var js,fjs=d.getElementsByTagName(s)[0];
+      	if(!d.getElementById(id)){js=d.createElement(s);
+      		js.id=id;js.src="//platform.twitter.com/widgets.js";
+      		fjs.parentNode.insertBefore(js,fjs);
+      	}
+      }(document,"script","twitter-wjs"));
 });
